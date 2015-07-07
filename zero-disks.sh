337dc467ff85c50d915ed246e78ef9dc1107395c
Robert Auch <thedoc@docsmooth.com>
2015-07-07 14:46:51 -0500

    Added script to zero out diskspace from https://lonesysadmin.net/2013/03/26/preparing-linux-template-vms/

---
+++ zero-disks.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+# From https://lonesysadmin.net/2013/03/26/preparing-linux-template-vms/
+
+# Determine the version of RHEL
+COND=`grep -i Taroon /etc/redhat-release`
+if [ "$COND" = "" ]; then
+        export PREFIX="/usr/sbin"
+else
+        export PREFIX="/sbin"
+fi
+
+FileSystem=`egrep '(ext|xfs)' /etc/mtab| awk -F" " '{ print $2 }'`
+
+for i in $FileSystem
+do
+        echo $i
+        number=`df -B 512 $i | awk -F" " '{print $3}' | grep -v Used`
+        echo $number
+        percent=$(echo "scale=0; $number * 99 / 100" | bc )
+        echo $percent
+        dd count=`echo $percent` if=/dev/zero of=`echo $i`/zf
+        /bin/sync
+        sleep 15
+        rm -f $i/zf
+done
+
+VolumeGroup=`$PREFIX/vgdisplay | grep Name | awk -F" " '{ print $3 }'`
+
+for j in $VolumeGroup
+do
+        VGFree=`$PREFIX/vgdisplay $j | awk -F" " '/Free/ { print $5 }'`
+        if [ ! "x$VGFree" = "x0" ]; then
+                echo $j
+                $PREFIX/lvcreate -l `$PREFIX/vgdisplay $j | grep Free | awk -F" " '{ print $5 }'` -n zero $j
+                if [ -a /dev/$j/zero ]; then
+                       cat /dev/zero > /dev/$j/zero
+                       /bin/sync
+                       sleep 15
+                       $PREFIX/lvremove -f /dev/$j/zero
+               fi
+        else
+                echo "$j has no free volume space, skipping."
+        fi
+done
+
