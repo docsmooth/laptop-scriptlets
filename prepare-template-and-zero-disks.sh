c7d8f9ee46cf5cd1a46be2544d94ce5c99e4f657
Robert Auch <thedoc@docsmooth.com>
2016-04-27 13:03:12 -0500

    renaming zero-disks.sh with additional Commands for preparing system from LoneSysAdmin

--- prepare-template-and-zero-disks.sh
+++ prepare-template-and-zero-disks.sh
@@ -1,6 +1,30 @@
 #!/bin/sh
 # From https://lonesysadmin.net/2013/03/26/preparing-linux-template-vms/
 
+/sbin/service/rsyslog stop
+/sbin/service/auditd stop
+/bin/package-cleanup --oldkernels --count=1
+yum clean all
+/usr/sbin/logrotate –f /etc/logrotate.conf
+/bin/rm -f /var/log/*-???????? /var/log/*.gz
+/bin/rm -f /var/log/dmesg.old
+/bin/rm -rf /var/log/anaconda
+/bin/cat /dev/null > /var/log/audit/audit.log
+/bin/cat /dev/null > /var/log/wtmp
+/bin/cat /dev/null > /var/log/lastlog
+/bin/cat /dev/null > /var/log/grubby
+/bin/rm -f /etc/udev/rules.d/70*
+/bin/sed -i '/^(HWADDR|UUID)=/d' /etc/sysconfig/network-scripts/ifcfg-eth0
+/bin/rm -rf /tmp/*
+/bin/rm -rf /var/tmp/*
+/bin/rm -f /etc/ssh/*key*
+/bin/rm -f ~root/.bash_history
+unset HISTFILE
+export HISTFILE
+/bin/rm -rf ~root/.ssh/known_hosts
+/bin/rm -f ~root/anaconda-ks.cfg
+touch /.unconfigured
+
 # Determine the version of RHEL
 COND=`grep -i Taroon /etc/redhat-release`
 if [ "$COND" = "" ]; then
