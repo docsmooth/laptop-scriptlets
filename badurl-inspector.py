ddcecdafc38832b4dfd27c94c1efa8d1bcf43cdd
Robert Auch <rob@kubuntu10.totalnetsolutions.net>
2017-05-11 10:50:00 -0500

    print up the 302 redirections, don't print w3 XML validation

--- badurl-inspector.py
+++ badurl-inspector.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
 import requests, re, sys
-r = requests.get("http://tnurl.net/bhje")
+requests.packages.urllib3.disable_warnings()
+try:
+    from requests.packages.urllib3.exceptions import InsecureRequestWarning
+    from requests.packages.urllib3.exceptions import SubjectAltNameWarning
+    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+    requests.packages.urllib3.disable_warnings(SubjectAltNameWarning)
+except ImportError:
+    if gInteractive:
+        print("WARNING: Your version of requests includes an older urllib3. If you are using '-i' you will still get warnings.")
 
-rx=re.compile('''https*:[^'">]*''', flags=re.U&re.I)
+#r = requests.get("http://pbps.totalnetsolutions.net", verify=False)
+
+rx=re.compile('''https*://(?!www.w3.org)[^'">]*''', flags=re.U&re.I)
 
 done={}
 
@@ -14,11 +24,13 @@ def printall(groups):
                 print(group)
                 if rx.match(group):
                     done[group]=True
-                    r=requests.get(group)
+                    r=requests.get(group, verify=False)
+                    if r.url != group:
+                        print(r.url)
                     j=rx.findall(r.text)
                     printall(j)
     else:
         print("wasn't passed a list, continuing.")
-print(sys.argv)
-printall(sys.argv)
+print(sys.argv[1:])
+printall(sys.argv[1:])
 
