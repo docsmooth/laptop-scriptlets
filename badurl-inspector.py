23c85413bce37f082798bb31159003841833af86
Robert Auch <rauch@totalnetsolutions.net>
2017-08-21 23:55:55 -0500

    fix up some stack traces that stopped execution to early. More pretty error messages, and now with comment.

--- badurl-inspector.py
+++ badurl-inspector.py
@@ -25,22 +25,35 @@ def printall(groups):
     if type(groups) == type([]):
         for group in groups:
             if not group in done:
+                # first print what we're going to try to get
                 print(group)
                 if rx.match(group):
                     done[group]=True
                     r=None
                     try:
                         r=requests.get(group, verify=False)
+                        # try to get the URL, catch the common errors and print pretty messages, so we can continue the rest of the list of URLs
                     except ConnectionResetError:
-                        print("Connection reset connecting to group!")
+                        print("Connection reset connecting to: {0}!".format(group))
+                        done[group]=True
+                    except requests.exceptions.ConnectionResetError:
+                        print("Connection reset connecting to: {0}!".format(group))
                         done[group]=True
                     except ConnectionError:
                         print("Generic connection error connecting to {0}!".format(group))
                         done[group]=True
+                    except requests.exceptions.ConnectionError:
+                        print("Generic connection error connecting to {0}!".format(group))
+                        done[group]=True
+                    except requests.exceptions.InvalidURL:
+                        print("Invalid URL error connecting to {0}!".format(group))
+                        done[group]=True
                     if r:
                         if r.url != group:
+                            #if we've been redirected somewhere other than what we tried to GET, print the resulting URL:
                             print(r.url)
                         j=rx.findall(r.text)
+                        # recurse to try to find more URLs till we fail.
                         printall(j)
     else:
         print("wasn't passed a list, continuing.")
