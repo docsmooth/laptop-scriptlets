d7bbdfeff281eb8c2ea883a9d985dacb30fee175
Robert Auch <rauch@totalnetsolutions.net>
2019-11-04 16:27:14 -0600

    fix up breaking each format into a stream

--- viss-normalizer.pl
+++ viss-normalizer.pl
@@ -3,9 +3,36 @@
 #
 use strict;
 use warnings;
+
+my %globalSeparators;
+my $filenum=0;
+my $prefix="format";
+
 while(<>) {
-    chomp;
-    my @x=split(/\b/, $_);
-    my @y=grep /[a-zA-Z0-9]/, @x;
-    print join("\t", @y), "\n";
+	chomp;
+	my @fields=split(/\b/, $_);
+	my @separatorGuesses=grep ! /[a-zA-Z0-9]/, @fields;
+    my %seps;
+	foreach my $sep (@separatorGuesses) {
+        if (not exists($seps{$sep})) { 
+            $seps{$sep}=1;
+        } else {
+            $seps{$sep}++;
+        }
+    }
+    my @topsep=sort { $seps{$a} <=> $seps{$b} } keys %seps;
+    if (not exists($globalSeparators{$topsep[0]})) {
+        if ( open (my $fh, ">", $prefix.$filenum.".txt")) {
+            $globalSeparators{$topsep[0]}->{"fh"}=$fh;
+        } else {
+            die "Could not open $prefix".$filenum.".txt to write!!";
+        }
+        $globalSeparators{$topsep[0]}->{"filename"}=$prefix.$filenum.".txt";
+        $filenum++;
+    }
+
+	print { $globalSeparators{$topsep[0]}->{fh}} ("$_\n");
+}
+foreach my $entry (keys(%globalSeparators)) {
+    close $globalSeparators{$entry}->{fh};
 }
