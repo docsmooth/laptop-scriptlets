2b94eae02c5cf67de68f6adc7d261117be096c85
Robert Auch <rauch@totalnetsolutions.net>
2019-11-19 05:39:57 -0600

    Add notes about requirements for MMA

--- Convert-NetshTrace.ps1
+++ Convert-NetshTrace.ps1
@@ -4,6 +4,13 @@ Param(
         [switch]$help
     ); #end param
 
+# Generate the netsh trace ETL file with:
+# netsh trace start persistent=(yes|no) capture=yes tracefile=c:\trace.etl
+# netsh trace stop
+#
+# This tool requires Microsoft Message Analyzer:
+# https://docs.microsoft.com/en-us/openspecs/blog/ms-winintbloglp/dd98b93c-0a75-4eb0-b92e-e760c502394f
+
 $etlExists = [System.IO.File]::Exists($etlfile);
 if ( $etlExists -eq $false)
 {
