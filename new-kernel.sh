668102490e32416de5ee83ea1f8e32db62438fcc
Robert Auch <thedoc@docsmooth.com>
2026-06-06 17:03:43 -0500

    svn update

--- new-kernel.sh
+++ new-kernel.sh
@@ -17,14 +17,6 @@ if [ -z "$version" ]; then
     exit 2
 fi
 oldpwd=`pwd`
-cd /usr/src/linux
-if [ $? -eq 0 ]; then
-    #we have an old kernel to clean up
-    oldkernel=`pwd -P`
-    cd /usr/src
-    rm -Rf $oldkernel
-    rm linux
-fi
 cd /usr/src
 base="linux-"
 if [ ! -f ./${base}${version}.tar.xz ]; then
@@ -36,6 +28,10 @@ if [ ! -f ./${base}${version}.tar.xz ]; then
         sudo chown -R $USER:src /usr/src/${base}${version}
         mv ./${base}${version}/${base}${version}.tar.bz2 /usr/src
         tarcmd="tar -jxf ./${base}$version.tar.bz2"
+    elif [ -f ./linux-source-$version.tar.bz2 ]; then
+        base="linux-source-"
+        sudo chown -R $USER:src /usr/src/${base}${version}
+        tarcmd="tar -jxf ./${base}${version}.tar.bz2"
     else
         echo $version | egrep -q '^4\.'
         if [ $? -eq 0 ]; then
@@ -50,6 +46,14 @@ if [ ! -f ./${base}${version}.tar.xz ]; then
             exit 2
         fi
         tarcmd="tar -Jxf ./${base}$version.tar.xz"
+    fi
+    cd /usr/src/linux
+    if [ $? -eq 0 ]; then
+        #we have an old kernel to clean up
+        oldkernel=`pwd -P`
+        cd /usr/src
+        rm -Rf $oldkernel
+        rm linux
     fi
 else
     echo "skipping download of new kernel - it already exists locally"
@@ -67,6 +71,11 @@ cd linux
 if [ $? -ne 0 -o $linkstatus -ne 0 ]; then
     echo "ERROR: could not update /usr/src/linux symclink to new version!"
     exit 2
+fi
+if [ ! -f /usr/src/linux/ubuntu/hio/Makefile ]; then
+    #bug in Ubuntu kernel sources 5.19.17
+    mkdir -p /usr/src/linux/ubuntu/hio
+    touch /usr/src/linux/ubuntu/hio/Makefile
 fi
 if [ -z "$oldconfig" ]; then
     currentkernel=`uname -r`
@@ -91,7 +100,8 @@ if [ ! -r "$oldconfig" ]; then
     echo "ERROR: Even after all that, we can't read $oldconfig!"
     exit 2
 fi
-cp $oldconfig /usr/src/linux/.config
+sed -e 's/CONFIG_LOCALVERSION=.*/CONFIG_LOCALVERSION=.'$TODAY'/' $oldconfig > /usr/src/linux/.config
+#cp $oldconfig /usr/src/linux/.config
 
 cd /usr/src/linux
 make oldconfig
@@ -107,13 +117,46 @@ echo "$compilecmd > $logfile"
 time $compilecmd > $logfile
 result=$?
 if [ "x$result" = "x0" ]; then 
+    buildresult=$result
+else
+    echo "ERROR: Build failed!  Check /usr/src/$TODAY.log"
+    exit 8
+fi
+# must sign kernel now...
+if [ -d /tmp/extracted-files ]; then
+    rm -Rf /tmp/extracted-files
+fi
+mkdir /tmp/extracted-files
+newpkg=`ls /usr/src/linux-image-$version{.,-}*.deb`
+if [ -z "$newpkg" -o ! -f "$newpkg" ]; then
+    # probably because the version is "5.19.0" instead of "5.19.17"
+    #TODO Fix this
+    newvers=`echo $version | sed -re 's/\.[[:digit:]]*$//'`
+    newpkg=`ls -rt /usr/src/linux-image-${newvers}{,.-}*.deb 2>/dev/null |tail -1`
+    if [ -z "$newpkg" -o ! -f "$newpkg" ]; then
+        echo "ERROR: Can't find new package /usr/src/linux-image-$version{,.-}*.deb"
+        echo "ERROR: Can't find new package /usr/src/linux-image-${newvers}{,.-}*.deb"
+        exit 8
+    fi
+    # rewrite version from 5.19.0 to 5.19.whatever so the packages match below
+    version=`echo $newpkg | sed -n -r -e 's/.*('${newvers}'\.[[:digit:]]*).*/\1/p'`
+fi
+fullvers=`echo $newpkg | sed -n -r -e 's/.*('${newvers}'\.[[:digit:]]*\.[[:digit:]]*).*/\1/p'`
+dpkg-deb -R $newpkg /tmp/extracted-files/
+cd /tmp
+sudo sbsign --key ~/workspace/codesigning/codesign.pem --cert ~/workspace/codesigning/codesign.pem extracted-files/boot/vmlinuz-$fullvers --output extracted-files/boot/vmlinuz-${fullvers}.signed
+sudo mv extracted-files/boot/vmlinuz-${fullvers}.signed extracted-files/boot/vmlinuz-${fullvers}
+sudo find /tmp/extracted-files/ -iname "*.ko" -exec kmodsign sha512 /var/lib/shim-signed/mok/MOK.priv ~/workspace/codesigning/MOK.der {} \;
+cd /tmp
+dpkg-deb -b extracted-files/ $newpkg
+df -h
+if [ "x$result" = "x0" ]; then 
     sudo dpkg -i /usr/src/linux-image-$version{.,-}*.deb /usr/src/linux-headers-$version.*.deb
     result=$?
 else
     echo "ERROR: Build failed!  Check /usr/src/$TODAY.log"
     exit 8
 fi
-df -h
 
 if [ "x$result" = "x0" ]; then
     echo "Installation of linux-image-$version successful."
